3b3d2f144a33120f35e0d1c212659d917511d25b
JSander72 <jamesearlsanders2@outlook.com>
2023-04-07 15:13:41 -0400

    thrid commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
+let boxOne = document.createElement("div");
+boxOne.id = "boxOne"
+
+let boxTwo = document.createElement("div");
+boxTwo.id = "boxTwo";
+boxTwo.className = "flexTheNotes";
+
+document.body.appendChild(boxOne);
+document.body.appendChild(boxTwo);
+
+
 document.addEventListener ("DOMContentLoaded", function(){
   let button = document.createElement("button");
   let btnText = document.createTextNode("Add Square");
@@ -11,6 +22,6 @@ document.addEventListener ("DOMContentLoaded", function(){
     blackBox.style.height = "1cm";
     blackBox.style.backgroundColor = "black"
     document.body.appendChild(blackBox)
-    
+
   })
 })
