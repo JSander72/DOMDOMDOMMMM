517b61962fef0cf5c2ecdfc65c1ed850fb3c2915
JSander72 <jamesearlsanders2@outlook.com>
2023-04-07 14:11:40 -0400

    thrid commit

--- scripts.js
+++ scripts.js
@@ -1,27 +1,16 @@
 document.addEventListener ("DOMContentLoaded", function(){
-  let paragraph = document.createElement("p");
-  let text = document.createTextNode("");
-
-  paragraph.style.color = "black";
-  paragraph.style.textTransform = "uppercase";
-  paragraph.className = "some-paragraph";
-
-  paragraph.appendChild(text);
-  document.body.appendChild(paragraph);
-
   let button = document.createElement("button");
   let btnText = document.createTextNode("Add Square");
   button.appendChild(btnText);
   document.body.appendChild(button);
 
-  let createBoxBtn = document.getElementById("createBoxBtn");
-  let boxContainer = document.getElementById("box-container")
 
- createBoxBtn.addEventListener("click",() => {
+ button.addEventListener("click",() => {
     let blackBox = document.createElement ("div");
     blackBox.style.width = "1cm";
     blackBox.style.height = "1cm";
     blackBox.style.backgroundColor = "black"
-    boxContainer.appendChild(blackBox);
+    document.body.appendChild(blackBox)
+    
   })
 })
