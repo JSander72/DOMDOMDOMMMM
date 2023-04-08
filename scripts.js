664bd725e988e6009267d4013dffa097f77c5a28
JSander72 <jamesearlsanders2@outlook.com>
2023-04-08 13:39:53 -0400

    second commit

--- scripts.js
+++ scripts.js
@@ -9,17 +9,28 @@ document.addEventListener ("DOMContentLoaded", function(){
   boxContainer.style.display = "flex";
   document.body.appendChild(boxContainer);
 
-  let counter = 0; // Counter variable
+  let counter = 0;
 
   button.addEventListener("click",() => {
-    counter++; // Increment counter
+    counter++;
 
     let blackBox = document.createElement("div");
-    blackBox.style.width = "5cm";
-    blackBox.style.height = "5cm";
+    blackBox.style.width = "150px";
+    blackBox.style.height = "150px";
     blackBox.style.backgroundColor = "black";
     blackBox.setAttribute("id", + counter);
-    blackBox.textContent = counter;
+    blackBox.setAttribute("class","boxes");
+
+
+    let idSpan = document.createElement("span");
+    idSpan.textContent = counter;
+    idSpan.style.width = "150px";
+    idSpan.style.height = "150px";
+    idSpan.style.justifyContent = "center";
+    idSpan.style.alignItems = "center";
+    idSpan.setAttribute("class","idNumberDisplay")
+    blackBox.appendChild(idSpan);
+
     boxContainer.appendChild(blackBox);
   });
 });
